50f8fc0eeb8e2e9f3817ff2079cf4504c3b34153
spencerhass <shass615@yahoo.com>
2015-11-17 11:44:43 -0500

    changes to spotify

--- app.js
+++ app.js
@@ -1,42 +1,54 @@
 $(document).ready(function() {
 	var audioObject = null;
+	$('.submit-button').click( getArtistId );
+});
 
 function getArtistId(query) {
-	var id = "";
-
-	var request = {
-		query: query,
-		type: 'artist'
-	};
+	var artist = $('input.artist').val(),
+			artistUrl = "https://api.spotify.com/v1/search?query="+artist+"&type=artist";
 
 	var result= $.ajax({
-		url:"https://api.spotify.com/v1/search",
-		data:request,
+		url: artistUrl,
 		dataType:'json',
 		type:'GET',
 	}).done(function(result) {
 		if(result.artists.items.length===0) {
 			showError("No results found for " + query);
 		}else {
-			id = result.artist.items[0].id;
+			id = result.artists.items[0].id;
 			getTopTracks(id);
 		}
 	}).fail(function(){
 		alert("Error getting artist");
-});
-	return id;
+	});
 }
 
 function getTopTracks(artistId) {
 		var result = $.ajax({
-			url: "https://api.spotify.com/v1/artists/" + artistId + "/top-tracks",
-			data: request,
+			url: "https://api.spotify.com/v1/artists/" + artistId + "/top-tracks?country=US",
 			dataType: "json",
 			type: "GET"
 		}).done(function(result) {
 			showResults(result);
 			return result;
-		}).fail(function() {
+		}).fail(function(result) {
+			console.log( result )
 			alert("Error getting tracks.");
 		});
+}
+
+function showResults( results )
+{
+	// console.log('results');
+	// console.log(results);
+	var newLineItem = '';
+	for (var i = 0;  i < results.tracks.length; i++)
+	{
+		newLineItem += '<li><img src="'+ results.tracks[i]['album']['images'][0]['url'] +'" /></li>';
+		newLineItem += '<li>'+ results.tracks[i]['album']['name'] +'</li>';
+		newLineItem += '<li><a href="'+ results.tracks[i]['preview_url'] +'" target="_blank">'+ results.tracks[i]['name'] +'</a></li>';
 	}
+	$('.results ul').append( newLineItem )
+
+
+}
